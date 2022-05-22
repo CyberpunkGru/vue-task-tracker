ac5d02b04a07387b7cbed885d7c38773ded96ab7
cyperpunkgru <grudev325@gmail.com>
2022-05-22 09:44:21 +0800

    wallet connection

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>Task Tracker</h1>
+<header>
+  <h1>{{ title }}</h1>
+  <Button text="Add Task" color="green"/>  
+</header>
 </template>
 
 <script>
+import Button from './Button.vue';
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: "Header",
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Header",
+    props: {
+        title: {
+            type: String,
+            default: "Hello World",
+        }
+    },
+    components: { Button }
 };
 </script>
 
